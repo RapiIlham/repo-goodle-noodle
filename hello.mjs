e90190ad3d2f05acadc0c08dd951efb864fb70a1
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-13 00:00:56 -0700

    Create hello.mjs

---
+++ hello.mjs
@@ -0,0 +1,18 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  const wssUrl = ws.upgradeReq.url;
+  console.log(`Client connected: ${wssUrl}`);
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
