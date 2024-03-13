ab4f45422f3d84976c475479d500b0d2f64627d9
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-13 06:05:35 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -26,9 +26,6 @@ const server = http.createServer(async (req, res) => {
             res.end('Error');
             con.close();
           }
-        } else {
-          res.writeHead(400);
-          res.end('Error');
         }
       }
     } else {
