ec128943ce191244821a46be21c7bf7046d0ba93
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-30 07:11:32 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -423,7 +423,7 @@ wss.on('connection',(client, req)=>{
       console.log('forward');
       var fd = new FormData();
       fd.append('id', req.url.split('-')[0].substr(1));
-      axios.post('https://71852867-7c3e-43d1-b105-c921b1a36268-00-dmdkgm8vaqdy.sisko.replit.dev/System/usedMinutes.php, fd).then((res) => {
+      axios.post('https://71852867-7c3e-43d1-b105-c921b1a36268-00-dmdkgm8vaqdy.sisko.replit.dev/System/usedMinutes.php', fd).then((res) => {
         console.log('forward success');
       }).catch(err => console.log(err););
     }
