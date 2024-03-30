60941073b63fb84999f2132defff4bcbc8b4fcca
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-30 06:56:59 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -420,6 +420,7 @@ wss.on('connection',(client, req)=>{
         broadcast(msg.toString(), req.url, 'host');
       }
     } else {
+      console.log('forward');
       request.post(
           'https://71852867-7c3e-43d1-b105-c921b1a36268-00-dmdkgm8vaqdy.sisko.replit.dev/System/usedMinutes.php',
           { json: { id: req.url.split('-')[0].substr(1) } },
