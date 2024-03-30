c8def3c9e32d1bc9e28ddf79bbbfcee5a08d55f3
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-30 07:12:54 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -425,7 +425,7 @@ wss.on('connection',(client, req)=>{
       fd.append('id', req.url.split('-')[0].substr(1));
       axios.post('https://71852867-7c3e-43d1-b105-c921b1a36268-00-dmdkgm8vaqdy.sisko.replit.dev/System/usedMinutes.php', fd).then((res) => {
         console.log('forward success');
-      }).catch(err => console.log(err););
+      }).catch(err => { console.log(err); });
     }
   })
 });
